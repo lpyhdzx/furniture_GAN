c7ca9e8066e6bb119c9acbb830769f446124dc9a
lpyhdzx <506302360@qq.com>
2018-05-06 20:09:06 +0800

    gpu is ok runing with 3*64*64

--- main.py
+++ main.py
@@ -12,27 +12,27 @@ from torchnet.meter import AverageValueMeter
 class Config(object):
     data_path = 'data/'  # 数据集存放路径
     num_workers = 4  # 多进程加载数据所用的进程数
-    image_size = 256  # 图片尺寸
-    batch_size = 8
+    image_size = 96  # 图片尺寸
+    batch_size = 4
     max_epoch = 200
     lr1 = 2e-4  # 生成器的学习率
     lr2 = 2e-4  # 判别器的学习率
     beta1 = 0.5  # Adam优化器的beta1参数
-    gpu = False  # 是否使用GPU
+    gpu = True  # 是否使用GPU
     nz = 100  # 噪声维度
-    ngf = 256  # 生成器feature map数
-    ndf = 256  # 判别器feature map数
+    ngf = 64  # 生成器feature map数
+    ndf = 64  # 判别器feature map数
 
     save_path = 'imgs/'  # 生成图片保存路径
 
     vis = True  # 是否使用visdom可视化
     env = 'GAN'  # visdom的env
-    plot_every = 20  # 每间隔20 batch，visdom画图一次
+    plot_every = 10  # 每间隔20 batch，visdom画图一次
 
     debug_file = '/tmp/debuggan'  # 存在该文件则进入debug模式
     d_every = 1  # 每1个batch训练一次判别器
     g_every = 5  # 每5个batch训练一次生成器
-    decay_every = 10  # 没10个epoch保存一次模型
+    decay_every = 5  # 每10个epoch保存一次模型
     netd_path=None
     netg_path=None
     # netd_path = 'checkpoints/netd_200.pth'  # 'checkpoints/netd_.pth' #预训练模型
@@ -41,8 +41,8 @@ class Config(object):
     # 只测试不训练
     gen_img = 'result.png'
     # 从512张生成的图片中保存最好的64张
-    gen_num = 5
-    gen_search_num = 512
+    gen_num = 8
+    gen_search_num = 32
     gen_mean = 0  # 噪声的均值
     gen_std = 1  # 噪声的方差
 
